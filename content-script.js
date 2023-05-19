bdb4f6e2bdc43e8e2235a64d4c88d80794655517
404salad <404salad@gmail.com>
2023-05-19 18:30:26 +0530

    Changed functionality

--- content-script.js
+++ content-script.js
@@ -4,9 +4,5 @@ addEventListener('keydown', event => {
   switch(event.key){
     case 'j': scrollBy(0, 50); break;
     case 'k': scrollBy(0, -50); break;
-    case 'h': history.back(); break;
-    case 'l': history.forward(); break;
-    case 't': chrome.runtime.sendMessage('t'); break;
-    case 'w': chrome.runtime.sendMessage('w'); break;
   }
 })
